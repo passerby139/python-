1f47f147cd21f5a0404b5bf2afacaec9f36b3a10
passerby139 <passerby18@163.com>
2019-11-18 23:02:37 +0800

    提交代码

---
+++ 最长公共前缀.py
@@ -0,0 +1,14 @@
+
+ class Solution:
+     def longestCommonPrefix(self, strs):
+         if not strs: return ""
+         s1 = min(strs)
+         s2 = max(strs)
+         for i, x in enumerate(s1):
+             if x != s2[i]:
+                 return s2[:i]
+         return s1
+
+
+
+
